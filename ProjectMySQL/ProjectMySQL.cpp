d9eda776037ad5ff17f3a99e59ab8daa8bf9286d
Wojtek Jastrzebowski <jastrzebowskiweb@gmail.com>
2022-08-16 10:48:01 +0200

    Adding more functions

--- ProjectMySQL/ProjectMySQL.cpp
+++ ProjectMySQL/ProjectMySQL.cpp
@@ -26,58 +26,46 @@ int main() {
         sql::PreparedStatement* pstmt;
         driver = get_driver_instance();
         con = driver->connect(server, username, password);
-//////////////Here, enter your Schema name, which you created earlier////////////////////////////////////
+//////////////Here, enter your Schema name, which you created earlier in your DB////////////////////////////////////
         con->setSchema("librarydb");
-//////////////Create tables, their columns, indexes, links, etc. for our DB//////////////////////////////
-
-
+//////////////Create tables, their columns, indexes, links, etc. for our libraryDB (if not existed)//////////////////////////////
+        cout << "Creating libraryDB tables...\n\n";
         stmt = con->createStatement();
-        stmt->execute("DROP TABLE IF EXISTS library_adress");
-        cout << "library_adress table removed (if existed)" << endl;
-        stmt->execute("CREATE TABLE library_adress (library_adress_ID INT(10) PRIMARY KEY NOT NULL, adress VARCHAR(50) NOT NULL, zipcode VARCHAR(25) NOT NULL, city VARCHAR(35) NOT NULL, country VARCHAR(25) NOT NULL);");
-        cout << "library_adress table has been created" << endl;
+        stmt->execute("CREATE TABLE IF NOT EXISTS library_adress (library_adress_ID INT(10) PRIMARY KEY NOT NULL, adress VARCHAR(50) NOT NULL, zipcode VARCHAR(25) NOT NULL, city VARCHAR(35) NOT NULL, country VARCHAR(25) NOT NULL);");
+        stmt->execute("CREATE TABLE IF NOT EXISTS employee (employee_ID INT(10) PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL, position VARCHAR(50) NOT NULL, username VARCHAR(50) NOT NULL, password VARCHAR(64) NOT NULL);");        
+        stmt->execute("CREATE TABLE IF NOT EXISTS employee_adress (employee_adress_ID INT(10) PRIMARY KEY NOT NULL, adress VARCHAR(50) NOT NULL, zipcode VARCHAR(25) NOT NULL, city VARCHAR(35) NOT NULL, country VARCHAR(25) NOT NULL);");  
+        stmt->execute("CREATE TABLE IF NOT EXISTS customer (customer_ID INT(10) PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL, registration_date DATE NOT NULL, status_toomuch VARCHAR(25), status_toolong VARCHAR(25));");
+        stmt->execute("CREATE TABLE IF NOT EXISTS customer_adress (customer_adress_ID INT(10) PRIMARY KEY NOT NULL, adress VARCHAR(50) NOT NULL, zipcode VARCHAR(25) NOT NULL, city VARCHAR(35) NOT NULL, country VARCHAR(25) NOT NULL);");
+        stmt->execute("CREATE TABLE IF NOT EXISTS book (book_ID INT(10) PRIMARY KEY NOT NULL, isbn VARCHAR(25) NOT NULL, title VARCHAR(50) NOT NULL, author VARCHAR(50) NOT NULL, publisher VARCHAR(25) NOT NULL, genre VARCHAR(25) NOT NULL, quantity_available INT(10) NOT NULL, status VARCHAR(25) NOT NULL);");
+        stmt->execute("CREATE TABLE IF NOT EXISTS rental_status (rental_status_ID INT(10) PRIMARY KEY NOT NULL, rent_customer_ID INT(10) NOT NULL, rent_book_ID INT(10) NOT NULL, rent_date DATE NOT NULL, CONSTRAINT FOREIGN KEY (rent_book_ID) REFERENCES book (book_ID), CONSTRAINT FOREIGN KEY (rent_customer_ID) REFERENCES customer (customer_ID));");
+        stmt->execute("CREATE TABLE IF NOT EXISTS return_status (return_status_ID INT(10) PRIMARY KEY NOT NULL, return_customer_ID INT(10) NOT NULL, return_book_ID INT(10) NOT NULL, return_date DATE NOT NULL, CONSTRAINT FOREIGN KEY (return_book_ID) REFERENCES book (book_ID), CONSTRAINT FOREIGN KEY (return_customer_ID) REFERENCES rental_status (rent_customer_ID));");
+        cout << "Welcome to libraryDB. Please logIn or Register as employee:\n\n";
+        cout << "1. LogIn\n"; 
+        cout << "2. Register\n";
+        cout << "0. Exit\n\n";
+        int option1;
+        switch (option1) {
+        case 1:
+            cout << "Your username: ";
+            cout << "Your password: ";
+            break;
+        case 2:
+            cout << "First name: ";
+            cout << "Name: ";
+            cout << "Your position: ";
+            cout << "Adress: ";
+            cout << "ZipCode: ";
+            cout << "City: ";
+            cout << "Country: ";
+            cout << "Your position: ";
+            break;
+        case 0:
+            // code block
+            break;
+        default:
+            // code block
+        }
 
-        stmt = con->createStatement();
-        stmt->execute("DROP TABLE IF EXISTS employee");
-        cout << "employee table removed (if existed)" << endl;
-        stmt->execute("CREATE TABLE employee (employee_ID INT(10) PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL, position VARCHAR(50) NOT NULL);");
-        cout << "employee table has been created" << endl;
-
-        stmt = con->createStatement();
-        stmt->execute("DROP TABLE IF EXISTS employee_adress");
-        cout << "employee_adress table removed (if existed)" << endl;
-        stmt->execute("CREATE TABLE employee_adress (employee_adress_ID INT(10) PRIMARY KEY NOT NULL, adress VARCHAR(50) NOT NULL, zipcode VARCHAR(25) NOT NULL, city VARCHAR(35) NOT NULL, country VARCHAR(25) NOT NULL);");
-        cout << "employee_adress table has been created" << endl;
-
-        stmt = con->createStatement();
-        stmt->execute("DROP TABLE IF EXISTS customer");
-        cout << "customer table removed (if existed)" << endl;
-        stmt->execute("CREATE TABLE customer (customer_ID INT(10) PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL, registration_date DATE NOT NULL, status_toomuch VARCHAR(25), status_toolong VARCHAR(25));");
-        cout << "customer table has been created" << endl;
-
-        stmt = con->createStatement();
-        stmt->execute("DROP TABLE IF EXISTS customer_adress");
-        cout << "customer_adress table removed (if existed)" << endl;
-        stmt->execute("CREATE TABLE customer_adress (customer_adress_ID INT(10) PRIMARY KEY NOT NULL, adress VARCHAR(50) NOT NULL, zipcode VARCHAR(25) NOT NULL, city VARCHAR(35) NOT NULL, country VARCHAR(25) NOT NULL);");
-        cout << "customer_adress table has been created" << endl;
-
-        stmt = con->createStatement();
-        stmt->execute("DROP TABLE IF EXISTS book");
-        cout << "book table removed (if existed)" << endl;
-        stmt->execute("CREATE TABLE book (book_ID INT(10) PRIMARY KEY NOT NULL, isbn VARCHAR(25) NOT NULL, title VARCHAR(50) NOT NULL, author VARCHAR(50) NOT NULL, publisher VARCHAR(25) NOT NULL, genre VARCHAR(25) NOT NULL, quantity_available INT(10) NOT NULL, status VARCHAR(25) NOT NULL);");
-        cout << "book table has been created" << endl;
-
-        stmt = con->createStatement();
-        stmt->execute("DROP TABLE IF EXISTS rental_status");
-        cout << "rental_status table removed (if existed)" << endl;
-        stmt->execute("CREATE TABLE rental_status (rental_status_ID INT(10) PRIMARY KEY NOT NULL, rent_customer_ID INT(10) NOT NULL, rent_book_ID INT(10) NOT NULL, rent_date DATE NOT NULL, CONSTRAINT FOREIGN KEY (rent_book_ID) REFERENCES book (book_ID), CONSTRAINT FOREIGN KEY (rent_customer_ID) REFERENCES customer (customer_ID));");
-        cout << "rental_status table has been created" << endl;
-
-        stmt = con->createStatement();
-        stmt->execute("DROP TABLE IF EXISTS return_status");
-        cout << "return_status table removed (if existed)" << endl;
-        stmt->execute("CREATE TABLE return_status (return_status_ID INT(10) PRIMARY KEY NOT NULL, return_customer_ID INT(10) NOT NULL, return_book_ID INT(10) NOT NULL, return_date DATE NOT NULL, CONSTRAINT FOREIGN KEY (return_book_ID) REFERENCES book (book_ID), CONSTRAINT FOREIGN KEY (return_customer_ID) REFERENCES rental_status (rent_customer_ID));");
-        cout << "return_status table has been created" << endl;
 
         delete stmt;
         delete con;
